4f3e555d0038a3b3b7cd27a58f6d82059c1d9c2d
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-25 13:01:05 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
-EC2_INSTANCE_ID=i-03df03631fa005b87
+EC2_INSTANCE_ID=i-05a1b1a64f057190c
 EC2_AZ=us-east-2c
 sudo sed -i "s/was deployed/was deployed on $EC2_INSTSNCE_ID in $EC2_AZ/g" /var/www/html/app.js
 chmod 664 /var/www/html/app.js
