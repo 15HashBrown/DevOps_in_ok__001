6b1d550adb8cd92f4ed7abff11e205d4e5b492f1
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-26 14:36:50 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -2,7 +2,7 @@
 
 cd /home
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-#. ~/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 sudo yum -y install nvm v16
 sudo nvm -y install node
 sudo yum -y install npm
