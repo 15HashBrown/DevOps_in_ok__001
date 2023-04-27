93861adbb7c711f17dc715ec0d0ef8f6626f4d92
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-27 09:28:40 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -5,6 +5,6 @@ cd /home
 
 
 sudo yum install -y gcc-c++ make 
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
+#curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
 
 sudo yum install -y nodejs 
