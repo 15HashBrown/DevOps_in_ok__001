a44bff7bb4a3bab531c1ed1d7b9c56a8e2597c88
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-27 09:22:47 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cd /home
-sudo yum -y install npm
+#sudo yum -y install npm
+
+
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
+
+sudo yum install -y nodejs 
