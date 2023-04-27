101267a534b0f276dd6b93c3d3e7b5a53caf7619
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-27 10:45:44 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -8,3 +8,6 @@ sudo yum install -y gcc-c++ make
 #curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
 
 sudo yum install -y nodejs --skip-broken
+
+#安裝app.js程式所需module
+sudo npm install express
