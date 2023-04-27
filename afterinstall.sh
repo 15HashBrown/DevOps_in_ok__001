c2a2b4267dcc3a2971cd8c3962788189604a6bf1
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-27 11:44:46 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -3,7 +3,7 @@
 cd /home
 #sudo yum -y install npm
 
-sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/enX0/disable_ipv6'
+#sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/enX0/disable_ipv6'
 
 sudo yum install -y gcc-c++ make 
 #curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - 
