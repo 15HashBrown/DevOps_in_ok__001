74b7104d7c064f979b608f7e4dad33567959ed74
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-05-02 17:00:44 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -14,7 +14,7 @@
 
 #安裝Nodejs 16, 因為是選擇Linux 2 AMI
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. /.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 nvm install 16
 
 #安裝app.js程式所需module
