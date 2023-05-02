a6b53572a48cba1b2932e17c973ce0869abe0bc5
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-05-02 13:38:06 +0800

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /home
-#sudo pm2 -y start server.js
-
-#node --inspect app.js
-
-#sudo npm install -g node-inspector
+cd /home/ec2-user
 node --inspect demo_server.js
+
+#sudo pm2 -y start server.js
+#node --inspect app.js
+#sudo npm install -g node-inspector
 #node --debug demo_server.js
