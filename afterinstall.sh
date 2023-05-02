c36f47592727d4f9b3a983268d6a769e0decad87
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-05-02 13:21:21 +0800

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -13,9 +13,9 @@
 #sudo yum install -y nodejs --skip-broken
 
 #安裝Nodejs 16, 因為是選擇Linux 2 AMI
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-#. ~/.nvm/nvm.sh
-#nvm install 16
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. /home/ec2-user/.nvm/nvm.sh
+nvm install 16
 
 #安裝app.js程式所需module
 #sudo npm install express
